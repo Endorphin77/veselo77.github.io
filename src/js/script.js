0c99e0f009988b8e7bc567d03514b96c5ccc681f
enorphine1998@yandex.ru <veselo1998@yandex.ru>
2021-04-05 23:32:46 +0300

    new

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.corusel__wrapper').slick({
+        speed: 1000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/left.png" alt="prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/right.png" alt="next"></button>',
+        fade: true,
+        cssEase: 'linear',
+        responsive: [
+            {
+              breakpoint: 860,
+              settings: {
+                arrows: false,
+                adaptiveHeight: false,
+                dots: true
+              }
+            }]
+    });
+  });
