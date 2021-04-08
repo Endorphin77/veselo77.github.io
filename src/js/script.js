098d81a17b3aff242dd8b6e5fcddaa45263dc377
enorphine1998@yandex.ru <veselo1998@yandex.ru>
2021-04-09 00:05:10 +0300

    nav3

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,20 @@
 $(document).ready(function(){
     $('.corusel__wrapper').slick({
         speed: 1000,
-        prevArrow: '<button type="button" class="slick-prev"><img src="img/left.png" alt="prev"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="img/right.png" alt="next"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/slick-prev.png" alt="prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/slick-next.png" alt="next"></button>',
         fade: true,
         cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 4000,
         responsive: [
             {
               breakpoint: 860,
               settings: {
                 arrows: false,
                 adaptiveHeight: false,
-                dots: true
+                dots: true,
+                
               }
             }]
     });
