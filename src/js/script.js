9d3cf929baefac45c95b2b839131d2e1a320ceb5
enorphine1998@yandex.ru <veselo1998@yandex.ru>
2021-04-13 22:48:21 +0300

    nav5

--- src/js/script.js
+++ src/js/script.js
@@ -50,3 +50,21 @@ $(document).ready(function(){
       $('.header__top-nav').removeClass('active');
     }
   });
+
+  window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+})
