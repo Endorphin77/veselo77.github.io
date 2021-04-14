fd1d990377df478f286f38b2164b2061fe10c4c9
enorphine1998@yandex.ru <veselo1998@yandex.ru>
2021-04-14 23:57:08 +0300

    nav6

--- src/js/script.js
+++ src/js/script.js
@@ -18,9 +18,9 @@ $(document).ready(function(){
               }
             }]
     });
-  });
+ 
 
-  $(document).ready(function(){
+
     $('.corusel__wrapper-products').slick({
         speed: 800,
         prevArrow: '<button type="button" class="slick-prev"><i class="bi bi-caret-left-fill"></i></button>',
@@ -32,39 +32,61 @@ $(document).ready(function(){
         slidesToScroll: 1,
         responsive: [
             {
-              breakpoint: 860,
+              breakpoint: 991,
               settings: {
                 slidesToShow: 2,
-                
-                
               }
-            }]
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+              }
+            }
+        ]
     });
-  });
+  
 
   $(document).scroll(function(){
     if ($(window).scrollTop() > 80){
       $('.header__top-nav').addClass('active');
+      $('.hamburger').addClass('hamburger_scroll');
     }
     else{
       $('.header__top-nav').removeClass('active');
+      $('.hamburger').removeClass('hamburger_scroll');
     }
   });
+});  
 
-  window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
+    function toggleSearch(item) {
+      $(item).each(function(i) {
+          $(this).on('click', function(e) {
+              e.preventDefault();
+              $('.search').eq(i).toggleClass('search_active');
+          })
+      });
+  };
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
-    });
+toggleSearch('.header__button');
+toggleSearch('.search__close');
+toggleSearch('.search__close-box');
 
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
-})
+
+    window.addEventListener('DOMContentLoaded', () => {
+      const menu = document.querySelector('.menu'),
+      menuItem = document.querySelectorAll('.menu_item'),
+      hamburger = document.querySelector('.hamburger');
+
+      hamburger.addEventListener('click', () => {
+          hamburger.classList.toggle('hamburger_active');
+          menu.classList.toggle('menu_active');
+      });
+
+      menuItem.forEach(item => {
+          item.addEventListener('click', () => {
+              hamburger.classList.toggle('hamburger_active');
+              menu.classList.toggle('menu_active');
+          })
+      })
+  })
